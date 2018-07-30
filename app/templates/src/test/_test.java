0d1424eaff0b76814e4ccc59ff0e1d0483d5ce51
javasparx <javasparx@gmail.com>
2018-07-31 01:29:25 +0500

    Fixed test template;

--- app/templates/src/test/_test.java
+++ app/templates/src/test/_test.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import uz.eopc.play.config.PlayConfiguration;
+import <%= basePackage %>.config.<%= appnameCapitalized %>Configuration;
 import <%= basePackage %>.service.<%= appnameCapitalized %>Service;
 
 import static org.junit.Assert.*;
